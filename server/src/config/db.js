ef03467ccbba08a510df4e2446a72b0493982d7a
Ayush Kumar <kayush3647@gmail.com>
2026-08-18 01:07:14 +0530

    add atlas

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,4 +1,25 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     const connection = await mongoose.connect(process.env.MONGO_URI);
+
+//     console.log(`MongoDB Connected: ${connection.connection.host}`);
+//   } catch (error) {
+//     console.error(`MongoDB Connection Failed: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers([
+  "1.1.1.1",
+  "8.8.8.8",
+]);
 
 const connectDB = async () => {
   try {

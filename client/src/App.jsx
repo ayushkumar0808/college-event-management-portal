b5c5d0e5ad7ba1c79c53f95dd6e61cce50e7d0df
Ayush Kumar <kayush3647@gmail.com>
2026-08-17 15:03:37 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
